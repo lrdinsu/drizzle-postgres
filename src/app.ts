6f63408b47603791dc29af576d4116639283247e
lilia <143701282+lrdinsu@users.noreply.github.com>
2024-01-06 23:24:54 -0800

    feat: Add one-to-one and one-to-many relations

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,163 @@
+import { and, eq, gt, gte, isNull, like, or } from 'drizzle-orm';
 import express, { Express } from 'express';
 
 import { db } from '@/db/index.js';
-import { user } from '@/db/schema.js';
+import { posts, profiles, users } from '@/db/schema.js';
+import { insertUserSchema } from '@/types/schema.js';
 
 export const app: Express = express();
 
 app.use(express.json());
 
 app.get('/api/v1/users', async (_, res) => {
-  const users = await db.select().from(user);
+  const allUsers = await db.select().from(users);
   res.status(200).json({
     status: 'success',
     data: {
-      users,
+      users: allUsers,
     },
   });
+});
+
+app.get('/api/v1/users/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    if (!id) {
+      res.status(400).json({
+        status: 'fail',
+        message: 'Please provide a user id',
+      });
+      return;
+    }
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(
+        and(
+          gte(users.id, 5),
+          or(gt(users.score, 70), like(users.fullName, '%e%')),
+        ),
+      );
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'User not found',
+    });
+  }
+});
+
+app.get('/api/v1/users/:id/profile', async (_, res) => {
+  // const id = Number(req.params.id);
+  // const results = await db.query.users.findMany({
+  //   columns: {
+  //     address: true,
+  //   },
+  //   where: ne(users.id, 4),
+  //   with: {
+  //     profile: true,
+  //   },
+  // });
+
+  const profiles2 = await db
+    .select({
+      id: profiles.id,
+      bio: profiles.bio,
+      userId: profiles.userId,
+      fullName: users.fullName,
+      address: users.address,
+    })
+    .from(users)
+    .leftJoin(profiles, eq(profiles.userId, users.id))
+    .where(isNull(users.address));
+
+  // const result = users.find((user) => user.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      full_profiles: profiles2,
+    },
+  });
+});
+
+app.post('/api/v1/users', async (req, res) => {
+  try {
+    const newUser = insertUserSchema.parse(req.body);
+    const insertedUser = await db
+      .insert(users)
+      .values(newUser)
+      .returning({ insertedId: users.id });
+
+    const newId = insertedUser[0].insertedId;
+    await db.insert(profiles).values({
+      userId: newId,
+      bio: 'Hello world',
+    });
+
+    res.status(201).json({
+      status: 'success',
+      message: 'New user created',
+      data: {
+        newUser,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(400).json({
+        status: 'fail',
+        message: err.message,
+      });
+      return;
+    }
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data',
+    });
+  }
+});
+
+app.get('/api/v1/users/:id/posts', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    if (!id) {
+      res.status(400).json({
+        status: 'fail',
+        message: 'Please provide a valid user id',
+      });
+      return;
+    }
+    // const results = await db.query.users.findFirst({
+    //   where: eq(users.id, id),
+    //   with: {
+    //     posts: true,
+    //   },
+    // });
+
+    const results = await db.query.posts.findFirst({
+      where: eq(posts.id, id),
+      with: {
+        author: true,
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        results,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'User not found',
+    });
+  }
 });
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,43 +1,62 @@
-import {
-  bigint,
-  bigserial,
-  boolean,
-  char,
-  date,
-  doublePrecision,
-  jsonb,
-  numeric,
-  pgEnum,
-  pgTable,
-  serial,
-  text,
-  time,
-  varchar,
-} from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   fullName: text('full_name').notNull(),
   phone: varchar('phone', { length: 256 }),
   address: varchar('address', { length: 256 }),
+  score: integer('score'),
 });
 
-export const moodEnum = pgEnum('mood', ['sad', 'ok', 'happy']);
-
-export const testTable = pgTable('test_table', {
-  id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-  qty: bigint('qty', { mode: 'number' }),
-  price: numeric('price', { precision: 7, scale: 2 }), //12345.67
-  score: doublePrecision('score'),
-  delivered: boolean('delivered'),
-  // description: text("description"),
-  description: varchar('description', { length: 256 }),
-  name: char('name', { length: 10 }), //"chair" => "chair      "
-  data: jsonb('data').notNull(),
-  startAt: time('start_at', {
-    precision: 3,
-    withTimezone: true,
-  }).defaultNow(),
-  date: date('date', { mode: 'date' }).defaultNow(),
-  mood: moodEnum('mood').default('happy'),
+export const profiles = pgTable('profiles', {
+  id: serial('id').primaryKey(),
+  bio: varchar('bio', { length: 256 }),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id),
 });
+
+export const posts = pgTable('posts', {
+  id: serial('id').primaryKey(),
+  text: varchar('text', { length: 256 }).notNull(),
+  authorId: integer('author_id')
+    .notNull()
+    .references(() => users.id),
+});
+
+export const usersRelations = relations(users, ({ one, many }) => ({
+  profile: one(profiles, {
+    fields: [users.id],
+    references: [profiles.userId],
+  }),
+  posts: many(posts),
+}));
+
+export const postsRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
+
+
+// export const moodEnum = pgEnum('mood', ['sad', 'ok', 'happy']);
+//
+// export const testTable = pgTable('test_table', {
+//   id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+//   qty: bigint('qty', { mode: 'number' }),
+//   price: numeric('price', { precision: 7, scale: 2 }), //12345.67
+//   score: doublePrecision('score'),
+//   delivered: boolean('delivered'),
+//   // description: text("description"),
+//   description: varchar('description', { length: 256 }),
+//   name: char('name', { length: 10 }), //"chair" => "chair      "
+//   data: jsonb('data').notNull(),
+//   startAt: time('start_at', {
+//     precision: 3,
+//     withTimezone: true,
+//   }).defaultNow(),
+//   date: date('date', { mode: 'date' }).defaultNow(),
+//   mood: moodEnum('mood').default('happy'),
+// });
